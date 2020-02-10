5b604489ce06d5bfdbdd92006b418d8a5de60f86
Matt C <mcompton2002@gmail.com>
2020-02-10 09:22:23 -0500

    Ex of "if" and "while"

---
+++ conditional.nim
@@ -0,0 +1,9 @@
+write(stdout,"What to test? : ")
+let compareMe = readLine(stdin)
+
+#echo compareMe
+
+if compareMe == "test":
+    echo "Nah"
+else:
+    echo "Wasn't prepared for this."
---
+++ loops.nim
@@ -0,0 +1,8 @@
+write(stdout,"What to test? : ")
+var input = readLine(stdin)
+
+while input != "rightanswer":
+    write(stdout,"Type the right answer : ")
+    input = readLine(stdin)
+
+echo "See, that wasn't so hard."
